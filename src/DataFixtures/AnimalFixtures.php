ea65614f0970a6ebcc4c25fd80705d4c8d78c1a4
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2019-11-30 12:57:55 +0100

    Creation des fixtures Famille et les lier avec les fixtures animaux + mettre à jour le BD

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\Famille;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,12 +11,32 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $f1 = new Famille();
+        $f1->setLibelle("Mammiféres")
+            ->setDescription("Animaux vertébrés nourrissent leurs petits avec du lait.")
+        ;
+        $manager->persist($f1);
+
+        $f2 = new Famille();
+        $f2->setLibelle("Reptiles")
+            ->setDescription("Animaux vertébrés qui rampent.")
+        ;
+        $manager->persist($f2);
+
+        $f3 = new Famille();
+        $f3->setLibelle("Poissons")
+            ->setDescription("Animaux invertébrés du monde aquatique.")
+        ;
+        $manager->persist($f3);
+
+
         $a1 = new Animal();
         $a1->setNom("Chien")
             ->setDescription("un animal domistique")
             ->setImage("chien.png")
             ->setPoids(10)
             ->setDangereux(false)
+            ->setFamille($f1);
         ;
         $manager->persist($a1);
 
@@ -25,6 +46,7 @@ class AnimalFixtures extends Fixture
             ->setImage("cochon.png")
             ->setPoids(300)
             ->setDangereux(false)
+            ->setFamille($f1);
         ;
         $manager->persist($a2);
 
@@ -34,6 +56,7 @@ class AnimalFixtures extends Fixture
             ->setImage("serpent.png")
             ->setPoids(5)
             ->setDangereux(true)
+            ->setFamille($f2);
         ;
         $manager->persist($a3);
 
@@ -43,6 +66,7 @@ class AnimalFixtures extends Fixture
             ->setImage("crocodile.png")
             ->setPoids(500)
             ->setDangereux(true)
+            ->setFamille($f2);
         ;
         $manager->persist($a4);
 
@@ -52,6 +76,7 @@ class AnimalFixtures extends Fixture
             ->setImage("requin.png")
             ->setPoids(800)
             ->setDangereux(true)
+            ->setFamille($f3);
         ;
         $manager->persist($a5);
 
